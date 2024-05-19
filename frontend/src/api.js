d3bf5b2083610c9509a62223fd73b393f0719293
Jayakkavin E <jayakkavin.e@payoda.net>
2024-05-19 19:41:07 +0530

    First Commit

---
+++ frontend/src/api.js
@@ -0,0 +1,24 @@
+//20
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
+//20
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+});
+
+
+//22
+api.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem(ACCESS_TOKEN);
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+  
+  export default api;
